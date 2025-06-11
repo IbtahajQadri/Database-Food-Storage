a8e631840ff2f284a6d5277fbd2bf53f496cd184
Hashim Hasnain Hadi <hah3609@thi.de>
2025-06-11 14:26:15 +0530

    Implemented the category feature

--- inventory/models.py
+++ inventory/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    unit = models.CharField(max_length=50)
+    ideal_quantity = models.FloatField()
+
+    def __str__(self):
+        return self.name
+
+
