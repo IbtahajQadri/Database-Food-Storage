3d68e21eed1dcc689e89cc5259dc49db8aaefb4a
Hashim Hasnain Hadi <hah3609@thi.de>
2025-06-14 13:40:41 +0000

    Update file admin.py

--- inventory/admin.py
+++ inventory/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Food
+from .models import Food, Category
 # Register your models here.
 
 admin.site.register(Food)
+admin.site.register(Category)
